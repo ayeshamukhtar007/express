33cc21f726f3e244404d7f6cc33753d8db36fb1c
ayeshamukhtar007 <you@example.com>
2022-07-16 22:19:57 +0500

    coneect DB

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+const connection = mongoose.connect('mongodb+srv://LMS:lms@cluster0.09hcl.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+var app = express();
+connection.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var Teacher = require('../models/teacher');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('index', { title: 'Express' });
 });
-
+router.post('/addteacher', function(req, res, next) {
+  Teacher.create(req.body)
+      .then((teacher) => {
+          console.log('Teacher has been Added ', teacher);
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(teacher);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+});
 module.exports = router;

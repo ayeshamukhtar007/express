1d44b8c07f8c0da168b446a752e70115e06dde21
ayeshamukhtar007 <you@example.com>
2022-07-17 20:23:36 +0500

    add TokenKey

--- routes/auth.js
+++ routes/auth.js
@@ -49,7 +49,7 @@ router.post('/login', async (req, res) => {
             {
                 id: user._id
             },
-            "MYNAMEISAYESHA",
+            process.env.JWT_SEC,
             { expiresIn: "3d" }
         );
   

99c33d38a6da8deb1c843d821285cf3ab3136ab1
ayeshamukhtar007 <you@example.com>
2022-07-18 00:21:08 +0500

    Up

--- routes/auth.js
+++ routes/auth.js
@@ -54,7 +54,7 @@ router.post('/login', async (req, res) => {
         );
   
         const { password, ...others } = user._doc;  
-        res.status(200).json({...others, accessToken,message:"login done"});
+        res.status(200).json({...others,accessToken,message:"login done"});
 
     }catch(err){
         res.status(500).json(err);

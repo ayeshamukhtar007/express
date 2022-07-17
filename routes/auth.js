edd3a6b29a86a5a4058ceedb1abb34f686a7029b
ayeshamukhtar007 <you@example.com>
2022-07-18 00:42:31 +0500

    up tokem

--- routes/auth.js
+++ routes/auth.js
@@ -49,7 +49,7 @@ router.post('/login', async (req, res) => {
             {
                 id: user._id
             },
-            process.env.JWT_SEC,
+            "MYNAMEISAYESHA",
             { expiresIn: "3d" }
         );
   

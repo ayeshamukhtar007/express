24b9a64eaff9f3ce4e1bd067ff9d281dc531e811
ayeshamukhtar007 <81950207+ayeshamukhtar007@users.noreply.github.com>
2022-12-07 00:54:08 +0500

    AddTimingAPI

--- routes/camera.js
+++ routes/camera.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Camera = require("../models/Camera");
+const Timings = require("../models/Timings");
 var ip = require("ip"); 
 const {
     verifyToken,
@@ -70,7 +71,22 @@ router.post("/addcamera/:uid",verifyTokenAndAuthorization, async (req, res) => {
   }
  
   });
+ router.post("/addmodeTimings/:uid",verifyTokenAndAuthorization, async (req, res) => {  
+  try{
+    
+    
+    
+       const new = new Timings(req.body);
+       
+       try {
+           const savedCamera=await new.save();
+           res.status(200).json({message:"set successfully"}); 
+       } catch (err) {
+           res.status(500).json({message:"failed"});
+       }
+  
  
+  });
 //UPDATE
 router.put("/updateCamera/:cid/:uid", verifyTokenAndAuthorization, async (req, res) => {
       console.log(req.body.status)  

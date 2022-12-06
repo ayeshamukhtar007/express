0c007aa96c1ff5da94dad22164e1d3f5b24ab420
ayeshamukhtar007 <81950207+ayeshamukhtar007@users.noreply.github.com>
2022-12-07 01:12:35 +0500

    changemodelName

--- models/Timing.js
+++ models/Timing.js
@@ -20,4 +20,4 @@ const TimingSchema = new mongoose.Schema(
   },
   { timestamps: true }
 )
-module.exports = mongoose.model("Timings",timingSchema);
+module.exports = mongoose.model("Timing",timingSchema);

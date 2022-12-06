2dc9f4b56b2ab414d2c33c1f2e66f27cd484f4e5
ayeshamukhtar007 <81950207+ayeshamukhtar007@users.noreply.github.com>
2022-12-07 00:57:00 +0500

    AddTimingModel

---
+++ models/Timing.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const TimingSchema = new mongoose.Schema(
+  {
+
+    activeStart: { 
+      type:string
+      
+     
+    },       
+    activeEnd:{
+        type:String
+    },
+    slientStart:{     
+      type:String
+    }
+,
+    slientEnd:{     
+      type:String
+    }
+  },
+  { timestamps: true }
+)
+module.exports = mongoose.model("Timings",timingSchema);
